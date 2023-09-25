0fe1f7f97327c12ab36343dbb6593120f8ffc541
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-09-26 01:17:13 +0800

    refactor(all): adopt new base interfaces from Component pkg (#28)
    Because
    
    - we refactored the base interface `IConnector` and `IExecution`
    
    This commit
    
    - adopt new base interfaces

--- pkg/main.go
+++ pkg/main.go
@@ -8,8 +8,10 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/types/known/structpb"
 
+	"github.com/instill-ai/component/pkg/base"
 	"github.com/instill-ai/connector-blockchain/pkg/numbers"
-	"github.com/instill-ai/connector/pkg/base"
+
+	connectorPB "github.com/instill-ai/protogen-go/vdp/connector/v1alpha"
 )
 
 var once sync.Once
@@ -49,12 +51,21 @@ func Init(logger *zap.Logger, options ConnectorOptions) base.IConnector {
 	return connector
 }
 
-func (c *Connector) CreateConnection(defUid uuid.UUID, config *structpb.Struct, logger *zap.Logger) (base.IConnection, error) {
+func (c *Connector) CreateExecution(defUid uuid.UUID, config *structpb.Struct, logger *zap.Logger) (base.IExecution, error) {
 	switch {
 
 	case c.numbersConnector.HasUid(defUid):
-		return c.numbersConnector.CreateConnection(defUid, config, logger)
+		return c.numbersConnector.CreateExecution(defUid, config, logger)
 	default:
 		return nil, fmt.Errorf("no blockchainConnector uid: %s", defUid)
+	}
+}
+
+func (c *Connector) Test(defUid uuid.UUID, config *structpb.Struct, logger *zap.Logger) (connectorPB.ConnectorResource_State, error) {
+	switch {
+	case c.numbersConnector.HasUid(defUid):
+		return c.numbersConnector.Test(defUid, config, logger)
+	default:
+		return connectorPB.ConnectorResource_STATE_ERROR, fmt.Errorf("no connector uid: %s", defUid)
 	}
 }
