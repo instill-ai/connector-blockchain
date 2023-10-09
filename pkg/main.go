a2723bc2a7d1b3c29700de84bc9dae6bc5b4d0a5
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-10-09 22:17:47 +0800

    refactor(component): adopt new base Connector struct and interface

--- pkg/main.go
+++ pkg/main.go
@@ -1,7 +1,6 @@
 package blockchain
 
 import (
-	"fmt"
 	"sync"
 
 	"github.com/gofrs/uuid"
@@ -18,54 +17,38 @@ var once sync.Once
 var connector base.IConnector
 
 type Connector struct {
-	base.BaseConnector
-	numbersConnector base.IConnector
+	base.Connector
+	connectorUIDMap map[uuid.UUID]base.IConnector
 }
 
-type ConnectorOptions struct {
-	Numbers numbers.ConnectorOptions
-}
-
-func Init(logger *zap.Logger, options ConnectorOptions) base.IConnector {
+func Init(logger *zap.Logger) base.IConnector {
 	once.Do(func() {
 
-		numbersConnector := numbers.Init(logger, options.Numbers)
-
 		connector = &Connector{
-			BaseConnector:    base.BaseConnector{Logger: logger},
-			numbersConnector: numbersConnector,
+			Connector:       base.Connector{Component: base.Component{Logger: logger}},
+			connectorUIDMap: map[uuid.UUID]base.IConnector{},
 		}
 
-		for _, uid := range numbersConnector.ListConnectorDefinitionUids() {
-			def, err := numbersConnector.GetConnectorDefinitionByUid(uid)
-			if err != nil {
-				logger.Error(err.Error())
-			}
-			err = connector.AddConnectorDefinition(uid, def.GetId(), def)
-			if err != nil {
-				logger.Warn(err.Error())
-			}
-		}
+		connector.(*Connector).ImportDefinitions(numbers.Init(logger))
 
 	})
 	return connector
 }
 
-func (c *Connector) CreateExecution(defUid uuid.UUID, config *structpb.Struct, logger *zap.Logger) (base.IExecution, error) {
-	switch {
-
-	case c.numbersConnector.HasUid(defUid):
-		return c.numbersConnector.CreateExecution(defUid, config, logger)
-	default:
-		return nil, fmt.Errorf("no blockchainConnector uid: %s", defUid)
+func (c *Connector) ImportDefinitions(con base.IConnector) {
+	for _, v := range con.ListConnectorDefinitions() {
+		err := c.AddConnectorDefinition(v)
+		if err != nil {
+			panic(err)
+		}
+		c.connectorUIDMap[uuid.FromStringOrNil(v.Uid)] = con
 	}
 }
 
+func (c *Connector) CreateExecution(defUID uuid.UUID, task string, config *structpb.Struct, logger *zap.Logger) (base.IExecution, error) {
+	return c.connectorUIDMap[defUID].CreateExecution(defUID, task, config, logger)
+}
+
 func (c *Connector) Test(defUid uuid.UUID, config *structpb.Struct, logger *zap.Logger) (connectorPB.ConnectorResource_State, error) {
-	switch {
-	case c.numbersConnector.HasUid(defUid):
-		return c.numbersConnector.Test(defUid, config, logger)
-	default:
-		return connectorPB.ConnectorResource_STATE_ERROR, fmt.Errorf("no connector uid: %s", defUid)
-	}
+	return c.connectorUIDMap[defUid].Test(defUid, config, logger)
 }
